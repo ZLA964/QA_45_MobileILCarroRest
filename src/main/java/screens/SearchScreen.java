c3aa71ec055578c150ae9880d1c25feb217e7b5d
לב לאונטייב <152332597+ZLA964@users.noreply.github.com>
2025-02-03 12:48:01 +0200

    try calendar

--- src/main/java/screens/SearchScreen.java
+++ src/main/java/screens/SearchScreen.java
@@ -3,12 +3,15 @@ package screens;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.TimeoutException;
 
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class SearchScreen extends BaseScreen {
 
@@ -35,6 +38,18 @@ public class SearchScreen extends BaseScreen {
     AndroidElement inputTo;
     @FindBy(id = "com.telran.ilcarro:id/searchBtn")
     AndroidElement btnYallaSearchCar;
+
+    @FindBy(id = "android:id/button1")
+    AndroidElement btnCalendarOk;
+
+    @FindBy(id = "android:id/date_picker_header_year")
+    AndroidElement btnYearCalendar;
+
+    @FindBy(id = "android:id/date_picker_year_picker")
+    AndroidElement calendarSelectYear;
+    @FindBy(xpath = "//*[@resource-id='android:id/date_picker_year_picker']/*[@resource-id='android:id/text1']")
+    List<AndroidElement> yearsOnCalendar;
+
 
     public void typeLocation(String taxt){
         inputLocation.sendKeys(taxt);
@@ -86,4 +101,44 @@ public class SearchScreen extends BaseScreen {
         clickWait(btnMoreOption, 5);
         clickWait(btnMyCars, 5);
     }
+
+    public void clickFrom(){
+        inputFrom.click();
+    }
+
+    public void clickTo(){
+        inputTo.click();
+    }
+
+    public void clickCalendarOk(){
+        btnCalendarOk.click();
+    }
+
+    public void setCalendarFromTO(String dayFrom, String dayTo) {
+        clickFrom();
+        setCalendar(dayFrom);
+        //       inputTo.click();
+        // setCalendar(dayTo);
+    }
+
+    private void setCalendar(String date) {
+        String[] dayArray = date.split("/");
+        String yearOfDay = dayArray[2];
+        String month = dayArray[1];
+        String day = dayArray[0];
+        clickWait(btnYearCalendar,3);
+        new WebDriverWait(driver,5).until(ExpectedConditions.presenceOfElementLocated(By.id( "android:id/date_picker_year_picker")));
+        System.out.println("Years found: " + yearsOnCalendar.size());
+        AndroidElement yearForClick = yearsOnCalendar.stream()
+                .filter( y-> yearOfDay.equalsIgnoreCase(y.getText()))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Year not found: " + yearOfDay));
+        yearForClick.click();
+        System.out.println("Year is selected!");
+        for (AndroidElement year : yearsOnCalendar) {
+            System.out.println("Year: " + year.getText());
+        }
+    }
+
+
 }
--- src/test/java/ui_mobile/SearchCarTests.java
+++ src/test/java/ui_mobile/SearchCarTests.java
@@ -27,16 +27,35 @@ public class SearchCarTests extends AppiumConfig {
     }
 
     @Test
-    public void searchCar_lvl_Test(){
+    public void searchCar_lvl_PositiveTest(){
         searchScreen.typeLocation("Rehovot");
         searchScreen.typeFrom("10/03/2025");
         searchScreen.typeTo("27/03/2025");
         searchScreen.clickBtnSearchCar();
         SearchResultScreen searchResultScreen = new SearchResultScreen(driver);
-//        System.out.println(searchResultScreen.validateOpenSearchResultScreen());
-//        System.out.println(searchResultScreen.notEmptyListOfCars());
+///        System.out.println(searchResultScreen.validateOpenSearchResultScreen());
+///        System.out.println(searchResultScreen.notEmptyListOfCars());
         Assert.assertTrue(searchResultScreen.validateOpenSearchResultScreen()
                 && searchResultScreen.notEmptyListOfCars());
+
+
+    }
+
+    @Test
+    public void searchCar_lvl_calendaPositiveTest(){
+        searchScreen.typeLocation("Rehovot");
+ //       searchScreen.clickFrom();
+        String dayFrom = "10/03/2026";
+        String dayTo = "27/04/2027";
+        searchScreen.setCalendarFromTO(dayFrom, dayTo);
+  //      searchScreen.typeFrom("10/03/2025");
+  //      searchScreen.typeTo("27/03/2025");
+  //      searchScreen.clickBtnSearchCar();
+        SearchResultScreen searchResultScreen = new SearchResultScreen(driver);
+///        System.out.println(searchResultScreen.validateOpenSearchResultScreen());
+///        System.out.println(searchResultScreen.notEmptyListOfCars());
+  //      Assert.assertTrue(searchResultScreen.validateOpenSearchResultScreen()
+    //            && searchResultScreen.notEmptyListOfCars());
 
 
     }
